a904ccd9db6e3abf97e2274fd6615b5eb33131cb
Carl Nathaniel <carlnathanielcenon2@gmail.com>
2026-08-22 19:20:46 +0800

    Added code for Activity 2

---
+++ src/com/joysistvi/stage1/day4/Activity2_Cenon.java
@@ -0,0 +1,26 @@
+package com.joysistvi.stage1.day4;
+
+public class Activity2_Cenon {
+
+    public static void main(String[] args) {
+
+        double income = 50000;
+        double foodAllowance = 15000;
+        double transportation = 2500;
+        double rent = 4500;
+        double utilityBill = 3500;
+        double remaining = income - foodAllowance - transportation - rent - utilityBill;
+
+        System.out.printf("Income: %.0f \n", income );
+        System.out.printf("Food Allowance: %.2f%% \n", foodAllowance / income * 100);
+        System.out.printf("Transportation: %.2f%% \n" , transportation / income * 100);
+        System.out.printf("Rent: %.2f%% \n", rent / income * 100);
+        System.out.printf("Utility Bill: %.2f%% \n", utilityBill / income * 100);
+        System.out.printf("Remaining Balance: %.0f \n", remaining);
+
+
+
+
+
+    }
+}
