415cbb0ec4cc0cdb7cf8cc4d1c557ba9292a33ac
Carl Nathaniel <carlnathanielcenon2@gmail.com>
2026-08-24 13:42:44 +0800

    Added code for Activity 3

--- src/com/joysistvi/stage1/day5/Activity3_Cenon.java
+++ src/com/joysistvi/stage1/day5/Activity3_Cenon.java
@@ -9,8 +9,41 @@ public class Activity3_Cenon {
 
         String allowed = age >= 18 && hasValidId ? "Yes" : "No";
 
-        System.out.println("Allowed to enter the event: " + allowed);
+        System.out.println("Task 2: Event Entry Check ");;
+        System.out.println("Allowed to enter the event: " + allowed );
 
+        //___________________________________________________________________
+
+
+        boolean isWeekend = true;
+        boolean isHoliday = false;
+
+        String noClasses = isHoliday || isWeekend ? "Yes, no Classes." : "No, we have classes today";
+
+        System.out.println("\nTask 3: Class Status Check");
+        System.out.println("Are there no classes today? " + noClasses);
+
+        //___________________________________________________________________
+
+
+        int score = 90;
+        String grades = (score > 75) ? "Passed" : "Failed";
+
+        System.out.println("\nTask 4: Pass or Fail Check");
+        System.out.println("Score: " + score);
+        System.out.println("Result: " + grades);
+
+        //___________________________________________________________________
+
+
+        int num1 = 100;
+        int num2 = 50;
+
+        int largerNumber = (num1 > num2) ? num1 : num2;
+
+        System.out.println("\nTask 5: Larger Number Check");
+        System.out.println("Number 1: "+ num1 + " " + "Number 2: " + num2);
+        System.out.println("The larger number is: " + largerNumber);
 
     }
 }
